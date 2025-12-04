cae73998a9eeb08339e839c8bf7a45bb7ee268f6
Royal-design <royaldesign1999@gmail.com>
2025-12-04 06:46:36 +0100

    updated

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -21,7 +21,6 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        // Ignore Firebase Timestamp warnings
         ignoredActions: ["auth/setUser", "auth/setUsers"],
         ignoredPaths: ["auth.user.updatedAt", "auth.users.updatedAt"]
       }
@@ -31,7 +30,6 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-// Define AppThunk type for asynchronous actions
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
